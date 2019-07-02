403132689351d036a205a5184494c45da3e03b2d
CLYP0103 <47965402+CLYP0103@users.noreply.github.com>
2019-07-02 15:00:58 +0800

    完成AI状态模式基本结构
    IAICharacter和IAIState

---
+++ StateMachine/Assets/Scripts/AI/AICharacter/IAICharacter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IAICharacter 
+{
+    //角色状态
+    protected IAIState m_AIState;
+
+    
+    //设置状态
+    public void ChangeState(IAIState _newAIState){
+        if(_newAIState==null){
+            Debug.LogError("IAICharacter.ChangeState(IAIState _newAIState): _newAIState is null!!!");
+            return;
+        }
+           
+        //Debug.Log状态
+        Debug.Log("改变状态："+_newAIState.StateName);
+
+
+        //退出当前状态
+        Debug.Log("结束状态："+m_AIState.StateName);
+        m_AIState.StateEnd();
+        //赋予新的状态
+        m_AIState = _newAIState;
+        //更新(状态拥有者)
+        _newAIState.SetAICharacter(this);
+        
+        //开始当前状态
+        Debug.Log("开始状态："+m_AIState.StateName);
+        m_AIState.StateStart();
+
+    }
+
+
+    //更新
+    public  void Update(){
+        m_AIState.StateUpdate();
+    }
+    
+}
---
+++ StateMachine/Assets/Scripts/AI/AIState/IAIState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IAIState 
+{
+    
+    //状态名称
+    private string m_StateName;
+    public string StateName{get;set;}
+
+
+
+    //状态拥有者
+    protected IAICharacter m_Character;
+
+
+
+    //设置状态拥有者
+    public void SetAICharacter(IAICharacter _newAICharacter){
+        m_Character=_newAICharacter;
+    }
+
+    //状态开始
+    public abstract void StateStart();
+
+    //状态更新
+    public abstract void StateUpdate();
+
+    //状态结束
+    public abstract void StateEnd();
+}
